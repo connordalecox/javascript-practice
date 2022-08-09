ee966c107dbc307a4a4dd6db68f2f895c04e9e5e
Connor Cox <connordalecox@gmail.com>
2022-08-08 21:01:08 -0700

    day 1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+no = false;
+yes = true;
+// boolean
+
+one = 1;
+// number
+
+abc = 'abc';
+a = 'a';
+// string
+
+
+
+// organization
+
+object = {
+    name: 'John',
+    age: 30,
+    isPresent: true
+}
+//object
+
+array = [1, 2, 3, 4, 5];
+// array
